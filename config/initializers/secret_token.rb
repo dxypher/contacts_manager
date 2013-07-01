8227d69754ee2cbbcfec028da45dbd4bce98862c
Nikhil <Home@c-24-5-28-219.hsd1.ca.comcast.net>
2013-07-01 16:08:22 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContactManager::Application.config.secret_token = '6b3e713cd29688f8ac3ce5f73d0696f3adab63a1df79bba838b2246922800aca8b3d3f44a48e6365d76603a3eec495d28bce2f60af4dc882ec6325bf4e493836'

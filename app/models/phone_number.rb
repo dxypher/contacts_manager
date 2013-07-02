5ae0290b005105fbf66271bd85e8661c51388793
Nikhil <Home@c-24-5-28-219.hsd1.ca.comcast.net>
2013-07-01 17:37:55 -0700

    add presence validation to phone number

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,3 +1,5 @@
 class PhoneNumber < ActiveRecord::Base
   attr_accessible :number, :person_id
+
+  validates_presence_of :number
 end
--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe PhoneNumber do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:phone_number) { PhoneNumber.new(number: '5555555555') }
+
+  it 'is valid' do
+    expect(phone_number).to be_valid
+  end
+
+  it 'is invalid without a number' do
+    phone_number.number = nil
+    expect(phone_number).not_to be_valid
+  end
 end

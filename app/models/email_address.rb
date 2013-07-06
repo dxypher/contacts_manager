22b78c56b553cf4ae5558550e0f70c75bc708ffb
Nikhil <Home@c-24-5-28-219.hsd1.ca.comcast.net>
2013-07-06 11:36:46 -0700

    add presence validation for person_id to EmailAddress

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -3,5 +3,5 @@ class EmailAddress < ActiveRecord::Base
 
   belongs_to :person
 
-  validates_presence_of :address
+  validates_presence_of :address, :person_id
 end
--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe EmailAddress do
-  let(:email_address) {EmailAddress.new(address: "john@example.com")}
+  let(:email_address) {EmailAddress.new(address: "john@example.com", person_id: 1)}
 
   it 'is valid' do
     expect(email_address).to be_valid
@@ -9,6 +9,11 @@ describe EmailAddress do
 
   it 'is not valid without an address' do
     email_address.address = nil
+    expect(email_address).to_not be_valid
+  end
+
+  it 'is not valid without a person_id' do
+    email_address.person_id = nil
     expect(email_address).to_not be_valid
   end
 end

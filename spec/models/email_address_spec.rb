66b9c9481f46ea5791e96e8c2e5d39d2968e8178
Nikhil <Home@c-24-5-28-219.hsd1.ca.comcast.net>
2013-07-06 11:32:06 -0700

    add presence validation to EmailAddress delete unused helper spec

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe EmailAddress do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:email_address) {EmailAddress.new(address: "john@example.com")}
+
+  it 'is valid' do
+    expect(email_address).to be_valid
+  end
+
+  it 'is not valid without an address' do
+    email_address.address = nil
+    expect(email_address).to_not be_valid
+  end
 end

7f5d79a2d6db1756bb33b1c4aa5092d9cf64b675
Nikhil <Home@c-24-5-28-219.hsd1.ca.comcast.net>
2013-07-06 11:24:40 -0700

    add EmailAddress model and relationship with person

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,6 +2,7 @@ class Person < ActiveRecord::Base
   attr_accessible :first_name, :last_name
 
   has_many :phone_numbers
+  has_many :email_addresses
 
   validates_presence_of :first_name, :last_name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130702002709) do
+ActiveRecord::Schema.define(:version => 20130706181855) do
+
+  create_table "email_addresses", :force => true do |t|
+    t.string   "address"
+    t.integer  "person_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "people", :force => true do |t|
     t.string   "first_name"

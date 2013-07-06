afac93951fbe6a799a11a8597ee20e97d62c949b
Nikhil <Home@c-24-5-28-219.hsd1.ca.comcast.net>
2013-07-06 16:29:43 -0700

    change EmailTable to have a contact_id and contact_type

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,5 +2,7 @@ class Company < ActiveRecord::Base
   attr_accessible :name
 
   has_many :phone_numbers, as: :contact
+  has_many :email_addresses, as: :contact
+  
   validates_presence_of :name
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -18,6 +18,11 @@ describe Company do
 
   it "responds with its phone numbers after they're created" do
     phone_numbers = company.phone_numbers.build(number: "555-3456")
-    expect(phone_numbers.number).to eq("555-3456")
+    expect(company.phone_numbers.map(&:number)).to eq(["555-3456"])
+  end
+
+  it "responds with its email addresses after they're created" do
+    email_addresses = company.email_addresses.build(address: 'hello@example.com')
+    expect(company.email_addresses.map(&:address)).to eq(['hello@example.com'])
   end
 end

cdc81007cfa6fd1340d687d1e065a74782b75039
Nikhil <Home@c-24-5-28-219.hsd1.ca.comcast.net>
2013-07-06 17:18:27 -0700

    remove unneeded controller actions, routes and specs

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -1,28 +1,5 @@
 class PhoneNumbersController < ApplicationController
-  # GET /phone_numbers
-  # GET /phone_numbers.json
-  def index
-    @phone_numbers = PhoneNumber.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @phone_numbers }
-    end
-  end
-
-  # GET /phone_numbers/1
-  # GET /phone_numbers/1.json
-  def show
-    @phone_number = PhoneNumber.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @phone_number }
-    end
-  end
-
-  # GET /phone_numbers/new
-  # GET /phone_numbers/new.json
   def new
     @phone_number = PhoneNumber.new(contact_id: params[:contact_id], contact_type: params[:contact_type])
 
@@ -32,13 +9,10 @@ class PhoneNumbersController < ApplicationController
     end
   end
 
-  # GET /phone_numbers/1/edit
   def edit
     @phone_number = PhoneNumber.find(params[:id])
   end
 
-  # POST /phone_numbers
-  # POST /phone_numbers.json
   def create
     @phone_number = PhoneNumber.new(params[:phone_number])
 
@@ -53,8 +27,6 @@ class PhoneNumbersController < ApplicationController
     end
   end
 
-  # PUT /phone_numbers/1
-  # PUT /phone_numbers/1.json
   def update
     @phone_number = PhoneNumber.find(params[:id])
 
@@ -69,8 +41,6 @@ class PhoneNumbersController < ApplicationController
     end
   end
 
-  # DELETE /phone_numbers/1
-  # DELETE /phone_numbers/1.json
   def destroy
     @phone_number = PhoneNumber.find(params[:id])
     @phone_number.destroy

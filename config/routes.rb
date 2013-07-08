440fb64decff639c7c68db8235bb1c332d414814
Nikhil <Home@c-24-5-28-219.hsd1.ca.comcast.net>
2013-07-08 16:00:54 -0700

    add login and logout links to application layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 ContactManager::Application.routes.draw do
-  root to: 'companies#index'
+  root to: 'site#index'
   resources :companies
   resources :email_addresses, except: [:index, :show]
   resources :phone_numbers, except: [:index, :show]
   resources :people
   get '/auth/:provider/callback' => 'sessions#create'
+  match '/login' => redirect("/auth/twitter"), as: :login
+  delete "/logout" => "sessions#destroy", as: :logout
 end
